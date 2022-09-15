c376c789d83b6256be8863fd3f6354ffa48a638b
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-09-15 23:24:11 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Tri [089618728196]
+│ • XL  [087892870587]
+│ • Dana  [089618728196]
+│ • Saweria  [https://saweria.co/nrkhlifah28]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6289618728196
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
