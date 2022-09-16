3c738d8a23fdc72abc5b392ec9e75e1c8c89676e
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-09-16 19:53:24 +0700

    Add files via upload

---
+++ plugins/o-getdb.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, text }) => {
+    m.reply('Tunggu Sebentar, Proses Getting File database.json')
+    let database = await fs.readFileSync('./database.json')
+    return await conn.sendMessage(m.chat, { document: database, mimetype: 'application/json', fileName: 'database.json' }, { quoted: m })
+}
+handler.help = ['getdb']
+handler.tags = ['host']
+handler.command = /^(g(et)?db)$/i
+
+handler.rowner = true
+
+module.exports = handler
