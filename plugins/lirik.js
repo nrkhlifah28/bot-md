8a6eb51104ceeac999210dbee25fa7fb94666366
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-09-17 17:24:15 +0700

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,3 +1,20 @@
+let finder = require('lyrics-finder')
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} kepastian rasa`
+  let error = `Maaf tidak di temukan lirik *${text}*`
+  let res = await finder("", text)
+  if (!res) throw error
+
+  m.reply(res)
+}
+handler.help = ['lirik'].map(v => v + ' <teks>')
+handler.tags = ['internet']
+handler.command = /^(lirik|lyrics|lyric)$/i
+
+module.exports = handler
+
+
+/*
 let fetch = require('node-fetch')
 let handler = async (m, { text, conn }) => {
   if(!text) throw `judulnya?`
